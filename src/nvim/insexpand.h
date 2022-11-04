6f57be3199b579244fb57dd8f057f5ab156a7115
ii14 <ii14@users.noreply.github.com>
2022-11-04 18:33:04 +0100

    Introduce runtime reflection and improve dynamism.

--- src/nvim/insexpand.h
+++
@@ -1,9 +0,0 @@
-#ifndef NVIM_INSEXPAND_H
-#define NVIM_INSEXPAND_H
-
-#include "nvim/vim.h"
-
-#ifdef INCLUDE_GENERATED_DECLARATIONS
-# include "insexpand.h.generated.h"
-#endif
-#endif  // NVIM_INSEXPAND_H
